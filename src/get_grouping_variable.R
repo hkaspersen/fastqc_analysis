0bb55c06e48b10ca061462f7b50f7decc31c3225
hkaspersen <hakon.kaspersen@protonmail.com>
2018-08-16 13:09:22 +0200

    new function that gets the group variables based on folders in the input directory

--- src/get_grouping_variable.R
+++ src/get_grouping_variable.R
@@ -1,7 +1,8 @@
 # Identifies the names of the files and groups them accoring to their respective folders
 get_grouping_variable <- function(path, folder) {
   files <- file_names(path, folder)
-  df <- data.frame(files = files, group = folder)
-  df$files <- gsub("(.*?)_fastqc.zip", "\\1", df$files)
+  df <- data.frame(files = files, group = folder, stringsAsFactors = FALSE)
+  df$files <- gsub("(.*?)_(.*?)_fastqc.zip", "\\1", df$files)
+  colnames(df) <- c("ref","group")
   return(df)
 }
